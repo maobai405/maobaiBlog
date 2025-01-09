7d0901fd25591c37a380c8a356fa8de9f70e0fdb
maobai405 <maobai405@gmail.com>
2025-01-09 08:35:19 +0800

    更新项目初始化md

--- src/config.ts
+++ src/config.ts
@@ -3,39 +3,40 @@ import type {
   NavBarConfig,
   ProfileConfig,
   SiteConfig,
-} from './types/config'
-import { LinkPreset } from './types/config'
+} from './types/config';
+import { LinkPreset } from './types/config';
 
 export const siteConfig: SiteConfig = {
   title: 'Maobaiの博客',
   subtitle: 'Blog Site',
-  lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
+  lang: 'zh_CN', // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
-    hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-    fixed: false,     // Hide the theme color picker for visitors
+    hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+    fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
     enable: true,
-    src: 'assets/images/banner.jpeg',   // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-    position: 'center',      // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+    src: 'assets/images/banner.jpeg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+    position: 'center', // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
     credit: {
-      enable: false,         // Display the credit text of the banner image
-      text: '',              // Credit text to be displayed
-      url: ''                // (Optional) URL link to the original artwork or artist's page
-    }
+      enable: false, // Display the credit text of the banner image
+      text: '', // Credit text to be displayed
+      url: '', // (Optional) URL link to the original artwork or artist's page
+    },
   },
   toc: {
-    enable: true,           // DisplBloggingay the table of contents on the right side of the post
-    depth: 2                // Maximum heading depth to show in the table, from 1 to 3
+    enable: true, // DisplBloggingay the table of contents on the right side of the post
+    depth: 2, // Maximum heading depth to show in the table, from 1 to 3
   },
-  favicon: [    // Leave this array empty to use the default favicon
+  favicon: [
+    // Leave this array empty to use the default favicon
     // {
     //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
     //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
     //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     // }
-  ]
-}
+  ],
+};
 
 export const navBarConfig: NavBarConfig = {
   links: [
@@ -44,14 +45,14 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/maobai405',     // Internal links should not include the base path, as it is automatically added
-      external: true,                               // Show an external link icon and will open in a new tab
+      url: 'https://github.com/maobai405', // Internal links should not include the base path, as it is automatically added
+      external: true, // Show an external link icon and will open in a new tab
     },
   ],
-}
+};
 
 export const profileConfig: ProfileConfig = {
-  avatar: 'assets/images/logo.jpeg',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+  avatar: 'assets/images/logo.jpeg', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
   name: 'MaoBai',
   bio: '',
   links: [
@@ -60,7 +61,7 @@ export const profileConfig: ProfileConfig = {
       // Visit https://icones.js.org/ for icon codes
       // You will need to install the corresponding icon set if it's not already included
       // `pnpm add @iconify-json/<icon-set-name>`
-      icon: 'fa6-brands:twitter',       
+      icon: 'fa6-brands:twitter',
       url: 'https://twitter.com',
     },
     {
@@ -74,10 +75,10 @@ export const profileConfig: ProfileConfig = {
       url: 'https://github.com/saicaca/fuwari',
     },
   ],
-}
+};
 
 export const licenseConfig: LicenseConfig = {
   enable: true,
   name: 'CC BY-NC-SA 4.0',
   url: 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-}
+};
